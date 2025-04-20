4a8877a736e88f5d53ddb93c7e57a2542337aa0a
martin <144384507+marnguye@users.noreply.github.com>
2025-04-20 16:55:31 +0200

    Add files via upload

---
+++ src/views/Post.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Post a Room</h2>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const title = ref('');
+const location = ref('');
+const rent = ref('');
+const utilties = ref('');
+const availability = ref('');
+const description = ref('');
+</script>
